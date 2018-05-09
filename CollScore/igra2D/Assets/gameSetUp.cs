153c543002dbf01aa0414444c81c27961d59c5ea
Mateo Pejić <mateo.pejic@fer.hr>
2018-05-09 23:21:36 +0200

    collision_score
    dodani kodovi za collision i score za TSP

---
+++ CollScore/igra2D/Assets/gameSetUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameSetUp : MonoBehaviour {
+
+    public Camera glavnaKamera;
+
+    public BoxCollider2D gornjiZid;
+    public BoxCollider2D donjiZid;
+
+    public Transform igrac;//Hocemo da 
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gornjiZid.size = new Vector2(glavnaKamera.ScreenToWorldPoint(new Vector3(Screen.width* 2f , 0f , 0f)).x, 1f);
+        gornjiZid.offset = new Vector2(glavnaKamera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, 0f, 0f)).x,
+                                    glavnaKamera.ScreenToWorldPoint(new Vector3(0f, Screen.height , 0f)).y + 0.5f);
+
+        donjiZid.size = new Vector2(glavnaKamera.ScreenToWorldPoint(new Vector3(Screen.width * 2f, 0f, 0f)).x, 1f);
+        donjiZid.offset = new Vector2(glavnaKamera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, 0f, 0f)).x,
+                                    glavnaKamera.ScreenToWorldPoint(new Vector3(0f, 0f, 0f)).y - 0.5f);
+    }
+}
